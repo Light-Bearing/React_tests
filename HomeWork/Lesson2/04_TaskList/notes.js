9e67ad5ce2482abd2cc936eab5d6707086c5b6f8
Light-Bearing <hilo@list.ru>
2017-12-01 16:32:54 +0300

    Завершил ДЗ №2-4 можно поковыряться со стилями чекбоксов но лень...

--- HomeWork/Lesson2/04_TaskList/notes.js
+++ HomeWork/Lesson2/04_TaskList/notes.js
@@ -5,42 +5,37 @@ var FilterControls = React.createClass({
   render: function() {
     return (
       <div className = "filter-controls" >
-        <label className = "label-radio" >
-          < input
-            type = "radio"
-            name = "filter"
-            onChange = {this.handleOnChangeFilter}
-            defaultChecked = {true}
-            value = "all"
-          />Все
-        </label>
-        <label className = "label-radio" >
-          <input
+        <span createClass="filter-control"><input
+              type = "radio"
+              name = "filter"
+              onChange = {this.handleOnChangeFilter}
+              defaultChecked = {true}
+              value = "all"
+              id="all"
+            /><label htmlFor="all" className = "label-radio" >Все</label></span>
+        <span createClass="filter-control"><input
             type = "radio"
             name = "filter"
             onChange = {this.handleOnChangeFilter}
             value = "checked"
-          />Завершенные
-        </label>
-        <label className = "label-radio" >
-          <input
-            type = "radio"
-            name = "filter"
-            onChange = {this.handleOnChangeFilter}
-            value = "unchecked"
-          />Не завершенные
-        </label>
+            id = "checked"
+          /><label htmlFor="checked" className = "label-radio" >Завершенные</label></span>
+          <span createClass="filter-control">
+            <input
+              type = "radio"
+              name = "filter"
+              onChange = {this.handleOnChangeFilter}
+              value = "unchecked"
+              id = "unchecked"
+            />
+          <label htmlFor="unchecked" className = "label-radio" >Не завершенные</label>
+        </span>
       </div>
     );
   }
 });
 
 var Task = React.createClass({
-  getInitialState: function() {
-    return {
-      checked: false
-    }
-  },
   onChange: function(event) {
 
     var task = {
@@ -48,109 +43,80 @@ var Task = React.createClass({
       checked: event.target.checked,
       text: this.props.children
     };
-    console.log(event.target);
     this.props.onChecked(task);
   },
-  /*  onClick:function(event){
-      this.setState({checked:event.target.checked})
-    },*/
+
   render: function() {
-
     return (
-
-      <
-      div className = "task" >
-      <
-      label >
-      <
-      input nums = {
-        this.key
-      }
-      type = "checkbox"
-      name = "task"
-      ref = "input"
-      defaultChecked = {
-        this.props.defchecked
-      }
-      onClick = {
-        this.onClick
-      }
-      onChange = {
-        this.onChange
-      }
-
-      />{this.props.children} <
-      /label> <
-      /div>
+      <div className = "task" >
+        <input
+            nums = {this.key}
+            type = "checkbox"
+            name = "task"
+            className = {"inp" + this.props.id}
+            defaultChecked = {this.props.defchecked}
+            onClick = {this.onClick}
+            onChange = {this.onChange}
+          />
+        <label htmlFor={"inp" + this.props.id}>
+          {this.props.children}
+        </label>
+        <span className="delete-task" onClick={this.props.onDelete}>×</span>
+      </div>
     );
   }
 });
-// onClick={onNoteDelete.bind(null, note)}
 
 var TaskList = React.createClass({
       render: function() {
         var onTaskCheckeds = this.props.onChecked;
+        var onTaskDelete = this.props.onTaskDelete;
         return ( <
           div className = "task-list" > {
             this.props.tasks.map(function(task) {
                 var results;
                 switch (this.props.filter) {
                   case "checked":
-                    if (task.checked) {
-                      results = < Task
-                      key = {
-                        task.id
-                      }
-                      id = {
-                        task.id
-                      }
-                      defchecked = {
-                        task.checked
-                      }
-                      onChecked = {
-                          this.props.onChecked
-                        } > {
-                          task.text
-                        } <
-                        /Task>;}
+                    if (task.checked)
+                    {
+                      results =
+                        <Task
+                          key = {task.id}
+                          id = {task.id}
+                          defchecked = {task.checked}
+                          onChecked = {this.props.onChecked}
+                          onDelete={onTaskDelete.bind(null, task)}
+                        >
+                          {task.text}
+                        </Task>;
+                    }
                       break;
                       case "unchecked":
                         if (!task.checked) {
-                          results = < Task
-                          key = {
-                            task.id
+                          results =
+                            <Task
+                              key = {task.id}
+                              id = {task.id}
+                              defchecked = {task.checked}
+                              onChecked = {this.props.onChecked}
+                              onDelete={onTaskDelete.bind(null, task)}
+                            >
+                              {task.text}
+                            </Task>;
                           }
-                          id = {
-                            task.id
-                          }
-                          defchecked = {
-                            task.checked
-                          }
-                          onChecked = {
-                              this.props.onChecked
-                            } > {
-                              task.text
-                            } <
-                            /Task>;}
                           break;
                           default: {
-                            results = < Task
-                            key = {
-                              task.id
+                            results =
+                              <Task
+                                key = {task.id}
+                                id = {task.id}
+                                defchecked = {task.checked}
+                                onChecked = {this.props.onChecked}
+                                onDelete={onTaskDelete.bind(null, task)}
+                              >
+                                {task.text}
+                              </Task>;
                             }
-                            id = {
-                              task.id
-                            }
-                            defchecked = {
-                              task.checked
-                            }
-                            onChecked = {
-                              this.props.onChecked
-                            } > {
-                              task.text
-                            } <
-                            /Task>;
-                          }
                         }
                         return results;
                     }.bind(this))
@@ -188,22 +154,17 @@ var TaskList = React.createClass({
         },
 
         render: function() {
-          return ( <
-            div className = "task-add" >
-            <
-            input type = "text"
-            placeholder = "Что сделать?"
-            onKeyPress = {
-              this.handleKeyPress
-            }
-            value = {
-              this.state.text
-            }
-            onChange = {
-              this.handleOnChange
-            }
-            /> <
-            /div>
+          return (
+            <div className = "task-add" >
+              <input
+                type = "text"
+                placeholder = "Что сделать?"
+                onKeyPress = {this.handleKeyPress}
+                value = {this.state.text}
+                onChange = {this.handleOnChange}
+                className="input-add"
+              />
+            </div>
           );
         }
       });
@@ -211,27 +172,32 @@ var TaskList = React.createClass({
       var TaskListApp = React.createClass({
         getInitialState: function() {
           return {
-            tasks: [{
-                id: 0,
-                checked: true,
-                text: "Выполнить ДЗ"
-              },
-              {
-                id: 1,
-                checked: false,
-                text: "Послать всех нах"
-              }
-            ],
+            tasks: [],
             filter: "all"
           };
         },
-
+        componentDidMount: function() {
+            var localtasks = JSON.parse(localStorage.getItem('tasks'));
+            if (localtasks) {
+                this.setState({ tasks: localtasks });
+            }
+        },
+        componentDidUpdate: function() {
+            this._updateLocalStorage();
+        },
         handleTaskAdd: function(newTask) {
           var newTasks = this.state.tasks.slice();
           newTasks.unshift(newTask);
           this.setState({
             tasks: newTasks
           });
+        },
+        handleTaskDelete: function(task) {
+            var taskId = task.id;
+            var newTasks = this.state.tasks.filter(function(task) {
+                return task.id !== taskId;
+            });
+            this.setState({ tasks: newTasks });
         },
 
         handleFiltred: function(filtred) {
@@ -248,37 +214,28 @@ var TaskList = React.createClass({
           }
           var newTasks = this.state.tasks.slice();
           newTasks[taskId] = task;
-          this.setState({
-            tasks: newTasks
-          });
+          this.setState({tasks: newTasks});
         },
 
         render: function() {
-          return ( <
-            div className = "task-app" >
-            <
-            h2 className = "app-header" > TaskList < /h2> <
-            TaskAdd onTaskAdd = {
-              this.handleTaskAdd
-            }
-            /> <
-            TaskList tasks = {
-              this.state.tasks
-            }
-            filter = {
-              this.state.filter
-            }
-            onChecked = {
-              this.handleTaskCheked
-            }
-            /> <
-            FilterControls onFiltred = {
-              this.handleFiltred
-            }
-            /> <
-            /div>
+          return (
+            <div className = "task-app" >
+              <h2 className = "app-header" >TaskList</h2>
+              <TaskAdd onTaskAdd = {this.handleTaskAdd}/>
+              <TaskList
+                tasks = {this.state.tasks}
+                filter = {this.state.filter}
+                onChecked = {this.handleTaskCheked}
+                onTaskDelete={this.handleTaskDelete}
+              />
+              <FilterControls onFiltred = {this.handleFiltred} />
+            </div>
           );
         },
+        _updateLocalStorage: function() {
+            var tasks = JSON.stringify(this.state.tasks);
+            localStorage.setItem('tasks', tasks);
+        }
       });
 
       ReactDOM.render( <
